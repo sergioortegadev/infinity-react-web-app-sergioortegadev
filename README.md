eb6c3b3fdd20a104c4bbec6f8dff3d13d1bff403
sergioortegadev <96734968+sergioortegadev@users.noreply.github.com>
2023-06-25 11:30:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# infinity-react-web-app-sergioortegadev
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-afyizs)
---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://sergioortega.com.ar" target="_blank">
+          <img
+            src="https://sergioortega.com.ar/assets/perfil.jpg"
+            className="logo"
+            alt="foto perfil"
+          />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Sergio Ortega Dev</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the profile photo and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
