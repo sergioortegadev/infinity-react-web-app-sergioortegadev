03b132fc9301b386185e83777447fbbfdc86ca4f
sergioortegadev <96734968+sergioortegadev@users.noreply.github.com>
2023-08-22 12:40:52 -0300

    1 file modified, 5 deleted

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,19 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://sergioortega.com.ar" target="_blank">
+        {/* <a href="https://sergioortega.com.ar" target="_blank">
           <img
             src="https://sergioortega.com.ar/assets/perfil.jpg"
             className="logo"
             alt="foto perfil"
           />
-        </a>
+        </a> */}
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Sergio Ortega Dev</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the profile photo and React logos to learn more
-      </p>
+      <h3>Web en mantenimiento</h3>
     </>
   );
 }
